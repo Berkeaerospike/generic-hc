7b9243a7c7cc2939ebcc76f1f6905bb62281a0c9
Berkeaerospike <110336050+Berkeaerospike@users.noreply.github.com>
2022-12-19 22:54:17 +0200

    Update generic-hc.sh

--- generic-hc.sh
+++ generic-hc.sh
@@ -2,7 +2,7 @@
 #Generic Healthcheck script
 # ./generic-hc.sh <path to collect info tgz>
 # commands list file should reside next to shell script
-# can write output to fiel > <name of output file
+# can write output to file > <name of output file
 # Example:./generic-hc.sh  ./collect_info_.tgz > ../PATH/... data.txt
 
 #INFO: Please use -v option for more details on failure. - IGNORE
